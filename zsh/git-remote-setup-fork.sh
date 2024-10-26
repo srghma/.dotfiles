2d3b2b982f1d5b0efd18d25a3d540891aa1776a6
Serhii Khoma <srghma@gmail.com>
2024-10-26 08:18:06 +0700

    feat: update

--- zsh/git-remote-setup-fork.sh
+++ zsh/git-remote-setup-fork.sh
@@ -5,3 +5,15 @@ git-remote-setup-fork () {
   git remote-upsert upstream "$origin"
   git remote-upsert origin "ssh://git@github.com/srghma/$reponame"
 }
+
+# reponame="idris2-lsp-vscode"
+# prid="dunhamsteve:markdown-code-blocks"
+# username=$(echo $prid | cut -d ':' -f 1) # srghma
+# branch=$(echo $prid | cut -d ':' -f 2)   # master
+# url="git@github.com:$username/$reponame.git"
+# # url="https://github.com/$username/$reponame/tree/$branch"
+# if git remote | grep -q "^$username$"; then operation="set-url" else operation="add" fi
+# echo git remote "$operation" "$username" "$url"
+# git remote "$operation" "$username" "$url"
+# git fetch "$username"
+# git merge "$username/$branch"
