1b7112f6766f327e9e8115aa12178dc25c7cb2f1
Serhii Khoma <srghma@gmail.com>
2025-01-06 17:18:47 +0700

    feat: update

--- bin/s6vnc.sh
+++ bin/s6vnc.sh
@@ -5,5 +5,6 @@ xrandr --newmode "2560x1600_60.00"  348.16  2560 2752 3032 3504  1600 1601 1604 
 xrandr --addmode HDMI-1 2560x1600_60.00
 xrandr --output HDMI-1 --mode 2560x1600_60.00 --left-of eDP-1
 x11vnc -clip 2560x1600+0+0
+xset r on
 
 xrandr --output HDMI-1 --off
