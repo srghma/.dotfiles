01d504e809ee387695f8a6c8490b3b73d51c22e8
Serhii Khoma <sergey@nordicresults.com>
2025-09-26 19:37:32 +0700

    feat: update

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,19 +2,27 @@ return {
   {
     "folke/snacks.nvim",
     opts = function(_, opts)
-      opts.picker = opts.picker or {}
-      opts.picker.win = opts.picker.win or {}
+      -- opts.picker = opts.picker or {}
+      opts.picker.layout = vim.tbl_deep_extend("force", opts.picker.layout or {}, {
+        fullscreen = true,
+      })
+      -- opts.picker.toggles = vim.tbl_deep_extend("force", opts.picker.toggles or {}, {
+      --   ignorecase = { icon = "I", value = false },
+      -- })
 
       -- Add to input window
+      opts.picker.win = opts.picker.win or {}
       opts.picker.win.input = opts.picker.win.input or {}
       opts.picker.win.input.keys = vim.tbl_deep_extend("force", opts.picker.win.input.keys or {}, {
         ["<M-r>"] = { "toggle_regex", mode = { "i", "n" } },
+        ["<M-i>"] = { "toggle_ignorecase", mode = { "i", "n" } },
       })
 
       -- Add to list window
       opts.picker.win.list = opts.picker.win.list or {}
       opts.picker.win.list.keys = vim.tbl_deep_extend("force", opts.picker.win.list.keys or {}, {
         ["<M-r>"] = "toggle_regex",
+        ["<M-i>"] = "toggle_ignorecase",
       })
 
       return opts
