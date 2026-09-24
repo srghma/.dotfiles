cde725eec347d8ccf06920727e8c30f32840fa4e
Serhii Khoma <sergey@nordicresults.com>
2026-09-24 10:02:29 +0700

    feat: update

--- NonEmpty/ArrayCorrectByConstruction.lean
+++
@@ -1,8 +0,0 @@
-module
-
-prelude
-public import NonEmpty.ArrayCorrectByConstruction.Basic
-public import NonEmpty.ArrayCorrectByConstruction.Ops
-public import NonEmpty.ArrayCorrectByConstruction.Instances
-public import NonEmpty.ArrayCorrectByConstruction.Notation
-public import NonEmpty.ArrayCorrectByConstruction.ToExpr
--- NonEmpty/String.lean
+++
@@ -1,6 +0,0 @@
-module
-prelude
-public import NonEmpty.String.Basic
-public import NonEmpty.String.ToExpr
-public import NonEmpty.String.Intercalate.ArrayCorrectByConstruction
-public import NonEmpty.String.Intercalate.ListCorrectByConstruction
